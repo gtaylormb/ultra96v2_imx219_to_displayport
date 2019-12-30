e015bef73870dc0a890156895e955357d78f6f83
taylorgr <taylorgr@beast>
2019-12-29 19:45:59 -0500

    Updated bitstream script to create xsa instead of hdf for Vitis

--- fpga/scripts/vivado_bitstream.tcl
+++ fpga/scripts/vivado_bitstream.tcl
@@ -4,4 +4,10 @@ open_checkpoint $outputDir/post_route.dcp
 
 write_bitstream -force $outputDir/imx219_to_mpsoc_displayport.bit
 write_debug_probes -force $outputDir/imx219_to_mpsoc_displayport.ltx
-write_sysdef -force -hwdef $outputDir/post_place.hwdef -bitfile $outputDir/imx219_to_mpsoc_displayport.bit -file $outputDir/imx219_to_mpsoc_displayport.hdf
+
+#Use this for Vivado 2019.1 (xsdk)
+#write_sysdef -force -hwdef $outputDir/post_place.hwdef -bitfile $outputDir/imx219_to_mpsoc_displayport.bit -file $outputDir/imx219_to_mpsoc_displayport.hdf
+
+#Use this for Vivado 2019.2 (vitis)
+set_property board_part em.avnet.com:ultra96v2:part0:1.0 [current_project]
+write_hw_platform -fixed -force -include_bit $outputDir/imx219_to_mpsoc_displayport.xsa
